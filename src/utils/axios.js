54b5f77b1c9d360521eab5924ec926147b0e4189
mattiacnn <mattiacannavo08@gmail.com>
2024-04-04 22:10:26 +0200

    Aggiornato l'URL di base per le chiamate API

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosServices = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3010/' });
+const axiosServices = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'https://energyapp-4k239xmu6-mattiacnn.vercel.app/api/' });
 
 // ==============================|| AXIOS - FOR MOCK SERVICES ||============================== //
 
